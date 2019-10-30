293cfa6f7421268955baafb00a91793f4717da5a
Ryan Goderis <rgoderis789@gmail.com>
2019-10-29 20:11:50 -0400

    added footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Footer (props){
+    return(
+        <footer class="page-footer font-small blue bg-info">
+            <div class="footer-copyright text-center py-3">
+                <p>Clicky Game</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
