62321e43da7142d3385371c53d77b5850b24aaca
Ryan Goderis <rgoderis789@gmail.com>
2019-10-30 09:45:33 -0400

    Updates

---
+++ precache-manifest.f8b1eeca8d9fa53a2fb4c9a3ecd71967.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7e219132fd9c4ca1f6886afca3ba08",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "0a85b1911c9842747833",
+    "url": "/clicky-game/static/css/main.74dc2bb6.chunk.css"
+  },
+  {
+    "revision": "8ddf23b8ae8e7f5c8847",
+    "url": "/clicky-game/static/js/2.19c06209.chunk.js"
+  },
+  {
+    "revision": "0a85b1911c9842747833",
+    "url": "/clicky-game/static/js/main.4c3ecb19.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
